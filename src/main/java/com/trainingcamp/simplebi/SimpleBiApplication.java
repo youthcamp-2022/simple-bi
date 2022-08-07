e8c8c85e1dfd0f94028f0d98cdc0830f5a19fb5a
zhenyue <zhenyuef@gmail.com>
2022-08-07 11:48:53 +0200

    Initial commit

---
+++ src/main/java/com/trainingcamp/simplebi/SimpleBiApplication.java
@@ -0,0 +1,12 @@
+package com.trainingcamp.simplebi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication public class SimpleBiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trainingcamp/simplebi/SimpleBiApplicationTests.java
@@ -0,0 +1,11 @@
+package com.trainingcamp.simplebi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SimpleBiApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
